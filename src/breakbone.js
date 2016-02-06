3e273aa1b010740c7c7c96da2c36f022d63d8c3d
Rodrigo Navarro <rnavarro1@gmail.com>
2016-02-06 12:29:31 -0200

    Removing mokeypatched methods.

--- src/breakbone.js
+++ src/breakbone.js
@@ -28,33 +28,5 @@ bb = (function() {
     }
   }
 
-  Array.prototype.remove = function(item) {
-    for (var i = 0, length = this.length; i < length; i++) {
-      if (this[i] === item) {
-        this.splice(i, 1);
-        return true;
-      }
-    }
-
-    return false;
-  }
-
-  Array.prototype.contains = function(item) {
-    for (var i = 0, length = this.length; i < length; i++) {
-      if (this[i] === item) {
-        return true;
-      }
-    }
-
-    return false;
-  }
-
-  // Fastest array clear implementation, see: http://jsperf.com/array-destroy/32
-  Array.prototype.clear = function() {
-    while (this.length > 0) {
-      this.pop();
-    }
-  }
-
   return bb;
 })();
